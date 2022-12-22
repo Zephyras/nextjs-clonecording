c64fb3900976c86e86887eb04b2fe291d3e2cf2d
zephyras <hkw0101@gmail.com>
2022-12-23 00:03:23 +0900

    Next js 강좌 #1 소개, 페이지 레이아웃 (Intro, Page layout) Next js / 강좌 #2 Dynamic Routes, next/link 완료
    Next js 강좌 #1 소개, 페이지 레이아웃 (Intro, Page layout) Next js / 강좌 #2 Dynamic Routes, next/link 완료

---
+++ src/components/Footer.js
@@ -0,0 +1,4 @@
+export default function Footer() {
+  return <div
+  style={{margintop: 40, padding: "40px 0", borderTop: "1px solid #999", textAlign: "center"}}>Copyright ⓒ 코딩앙마. All rights reserved.</div>
+}
---
+++ src/components/itemList.js
@@ -0,0 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
+import { Grid } from 'semantic-ui-react'
+import styles from './ItemList.module.css';
+import Link from "next/link";
+import { Component } from 'react';
+
+export default function ItemList({ list }) {
+  return (
+    <div>
+      <Grid columns={3}>
+        <Grid.Row>
+          {list.map(item => (
+            // eslint-disable-next-line react/jsx-key
+            <Grid.Column key={item.id}>
+              <Link href={`/view/${item.id}`}>
+              
+                <div className={styles.wrap}>
+                  <img 
+                    src={item.image_link} 
+                    alt={item.name} 
+                    className={styles.img_item} 
+                    />
+                  <strong className={styles.tit_item}>{item.name}</strong>
+                  <span className={styles.txt_info}>
+                    {item.category} {item.product_type}
+                  </span>
+                  <strong className={styles.num_price}>${item.price}</strong>
+                </div>
+              
+              </Link>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
