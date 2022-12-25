13ec8d642b286471ddf42865f29eba98f5304111
zephyras <hkw0101@gmail.com>
2022-12-25 10:38:58 +0900

    sement-ui-react link 열 맞춤 수정 및 CSS수정
    sement-ui-react link 열 맞춤 수정 및 CSS수정

--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,29 @@
-import {Button, Divider, Form, Header, List, TextArea} from "semantic-ui-react"
+import { Button, Divider, Form, Header, List, TextArea } from "semantic-ui-react"
 export default function About() {
   return (
     <>
-      <Header as="h3" style={{padding:40}} color="teal">
+      <Header as="h3" style={{ paddingTop: 40 }} color="teal">
         회사소개
       </Header>
 
-      <Divider/>
+      <Divider />
       <List>
-          <List.Item>
-            <List.Icon name="users"/>
-            <List.Content>Sementic UI</List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name="marker"/>
-            <List.Content>New York, NY</List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Icon name="mail"/>
-            <a href="mailto:hkw0101@gmail.com">hkw0101@gmail.com</a>
-          </List.Item>
-          <List.Icon name="linkify" />
-          <List.Content>
-            <a href="http://www.semantic-ui.com">semantic-ui.com</a>
-          </List.Content>
+        <List.Item>
+          <List.Icon name="users" />
+          <List.Content>Sementic UI</List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon name="marker" />
+          <List.Content>New York, NY</List.Content>
+        </List.Item>
+        <List.Item
+          icon='mail'
+          content={<a href='mailto:jack@semantic-ui.com'>jack@semantic-ui.com</a>}
+        />
+        <List.Item 
+        icon="linkify" 
+        content={<a href="http://www.semantic-ui.com">semantic-ui.com</a>} />
+      
       </List>
       <Header as="h3" style={{ paddingTop: 40 }} color="teal">
         문의 사항
@@ -38,7 +38,7 @@ export default function About() {
           <label>내용</label>
           <TextArea />
         </Form.Field>
-        <Button style = {{marginBottom: 20}} color="green">보내기</Button>
+        <Button style={{ marginBottom: 20 }} color="green">보내기</Button>
       </Form>
     </>
   )
